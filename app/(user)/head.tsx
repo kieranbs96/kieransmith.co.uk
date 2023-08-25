e9685cfdabad3460eec63e379ad91ba92b122971
Kieran.Smith <Kieran.Smith@global.com>
2023-08-25 12:04:48 +0100

    bugfix: debug 3

--- app/(user)/head.tsx
+++ app/(user)/head.tsx
@@ -1,9 +1,9 @@
-// import { DefaultTags } from 'components/DefaultTags';
+import { DefaultTags } from 'components/DefaultTags';
 
 export default function Head() {
   return (
     <>
-      {/* <DefaultTags /> */}
+      <DefaultTags />
       <title>Kieran Smith | Frontend Developer</title>
       <meta name="description" content="Kieran Smith - Frontend Developer" />
       {/* <link rel="stylesheet" href="https://rsms.me/inter/inter.css" /> */}
